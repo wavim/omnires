4180c243d09fc8995c7055d019c617604302f2e9
carbonsoda <carbonsoda@outlook.com>
2025-07-21 16:40:26 +0800

    v2.2.1 fix Element class for SVGs

--- src/generic/components/element.tsx
+++ src/generic/components/element.tsx
@@ -4,12 +4,12 @@ import { Colored } from "../colored";
 export const OmniElement: OmniGeneric<Element> = (props) => {
 	const ele = props.children;
 
+	const cls = ele.classList.length !== 0 ? `.${Array.from(ele.classList).join(".")}` : "";
+
 	return (
 		<Colored color="oklch(52.5% 0.223 3.958)">
 			{`${ele.localName}${ele.id ? `#${ele.id}` : ""}`}
-			<Colored color="oklch(70.5% 0.213 47.604)">
-				{ele.className ? `.${Array.from(ele.classList).join(".")}` : ""}
-			</Colored>
+			<Colored color="oklch(70.5% 0.213 47.604)">{cls}</Colored>
 		</Colored>
 	);
 };
